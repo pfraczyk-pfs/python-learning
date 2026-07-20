1fc937c274c7a8cbb1c57dfa1527cb147ddbb500
Piotr Frączyk <piotr.fraczyk@pm.me>
2026-07-20 17:59:16 +0200

    Add reusable file category classification

--- projects/downloads_organizer/main.py
+++ projects/downloads_organizer/main.py
@@ -1,9 +1,44 @@
-from pathlib import Path
+from pathlib import Path   
 
+def get_category(extension,extension_map):
+    return extension_map.get(extension, "Other")
+
+extension_map = {
+    '.doc': 'Documents',
+    '.docx': 'Documents',
+    '.pdf': 'Documents',
+    '.csv': 'Documents',
+    '.xls': 'Documents',
+    '.xlsx': 'Documents',
+    '.pptx': 'Documents',
+
+    '.jpg': 'Images',
+    '.png': 'Images',
+    '.svg': 'Images',
+
+    '.mp4': 'Videos',
+    '.mkv': 'Videos',
+
+    '.mp3': 'Audio',
+    '.flac': 'Audio',
+
+    '.rar': 'Archives',
+    '.zip': 'Archives',
+    '.7z': 'Archives',
+
+    '.exe': 'Installers',
+    '.msi': 'Installers',
+
+    '.py': 'Code',
+    '.json': 'Code',
+    '.yaml': 'Code',
+    '.xml': 'Code'
+}
 downloads_path = Path.home() / "Downloads"
 
 print(f"Scanning folder: {downloads_path}")
 
 for file_path in downloads_path.iterdir():
     if file_path.is_file():
-        print(file_path.name, "->", file_path.suffix)
+        category = get_category(file_path.suffix, extension_map)
+        print(file_path.name, "->", category)
